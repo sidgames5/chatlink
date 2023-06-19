b4d6223c096dced5f3324240e7f5cee02803cbbb
BasicCorruption <saytleader@outlook.com>
2023-06-19 16:51:53 -0400

    Make bukkit plugin

---
+++ src/main/java/com/sidgames5/chatlink/Chatlink_bukkit_1_0.java
@@ -0,0 +1,17 @@
+package com.sidgames5.chatlink;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chatlink_bukkit_1_0 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
