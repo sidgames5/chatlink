5e167a422d62e2b57682cc17398501186d2bfaee
BasicCorruption <saytleader@outlook.com>
2023-06-19 20:17:04 -0400

    Change version

--- src/main/java/com/sidgames5/chatlink/Main.java
+++ src/main/java/com/sidgames5/chatlink/Main.java
@@ -12,7 +12,7 @@ import javax.security.auth.login.LoginException;
 
 public final class Main extends JavaPlugin {
     private static final Logger logger = LoggerFactory.getLogger("ChatLink");
-    public static final String VERSION = "1.0.1+bukkit";
+    public static final String VERSION = "1.0.1+bukkit-1.19";
 
     @Override
     public void onEnable() {
