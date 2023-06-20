fc9bc1614b2d4c7e8ea42a7b3558d26440a13567
BasicCorruption <saytleader@outlook.com>
2023-06-19 20:22:26 -0400

    Final preparations for public release

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# chatlink
+# ChatLink
+
 A simple mod to link your server and discord chat
+
+## Versions
+
+| Mod Version | Supported Minecraft Versions  |
+|-------------|-------------------------------|
+| 1.0.0       | 1.19.x, 1.20.x                |
+
+## Installation
--- src/main/java/com/sidgames5/chatlink/Main.java
+++ src/main/java/com/sidgames5/chatlink/Main.java
@@ -12,7 +12,7 @@ import javax.security.auth.login.LoginException;
 
 public final class Main extends JavaPlugin {
     private static final Logger logger = LoggerFactory.getLogger("ChatLink");
-    public static final String VERSION = "1.0.1+bukkit-1.19";
+    public static final String VERSION = "1.0.0+bukkit-1.19";
 
     @Override
     public void onEnable() {
@@ -22,8 +22,9 @@ public final class Main extends JavaPlugin {
         logger.info("Loading config");
         PluginConfig.setup();
         PluginConfig.save();
-        PluginConfig.getConfig().addDefault("botToken", "Place your bot token in here");
+        PluginConfig.getConfig().addDefault("BOT_TOKEN", "Place your bot token in here");
         PluginConfig.getConfig().addDefault("channelID", "Place your discord channel ID in here");
+        PluginConfig.getConfig().addDefault("webhook", "Place your discord webhook URL in here");
         PluginConfig.save();
         logger.info("Config loaded");
 
