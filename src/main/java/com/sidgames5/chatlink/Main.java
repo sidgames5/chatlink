4ac1d4cdec602829158eeeea455b3255697d379c
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-06-22 22:13:37 -0400

    Update version

--- src/main/java/com/sidgames5/chatlink/Main.java
+++ src/main/java/com/sidgames5/chatlink/Main.java
@@ -12,7 +12,7 @@ import javax.security.auth.login.LoginException;
 
 public final class Main extends JavaPlugin {
     private static final Logger logger = LoggerFactory.getLogger("ChatLink");
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     @Override
     public void onEnable() {
