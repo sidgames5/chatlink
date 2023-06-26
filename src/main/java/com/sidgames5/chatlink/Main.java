0691578aa143aed98f1405c07f51a0cd21353236
BasicCorruption <saytleader@outlook.com>
2023-06-26 14:18:16 -0400

    Bump version

--- src/main/java/com/sidgames5/chatlink/Main.java
+++ src/main/java/com/sidgames5/chatlink/Main.java
@@ -12,7 +12,7 @@ import javax.security.auth.login.LoginException;
 
 public final class Main extends JavaPlugin {
     private static final Logger logger = LoggerFactory.getLogger("ChatLink");
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
 
     @Override
     public void onEnable() {
